d37322f93d3d4fc3c22f0d8ec0288b2a0c0b10e3
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-01-02 19:24:18 +0100

    Ormconfig

--- src/season-players/season-players.service.ts
+++ src/season-players/season-players.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateSeasonPlayerDto } from './dto/create-season-player.dto';
 import { UpdateSeasonPlayerDto } from './dto/update-season-player.dto';
+import { SeasonPlayer } from './entities/season-player.entity';
 
 @Injectable()
 export class SeasonPlayersService {
+  constructor(
+    @InjectRepository(SeasonPlayer)
+    private readonly seasonPlayerRepository: Repository<SeasonPlayer>,
+  ) {}
+
   create(createSeasonPlayerDto: CreateSeasonPlayerDto) {
     return 'This action adds a new seasonPlayer';
   }
